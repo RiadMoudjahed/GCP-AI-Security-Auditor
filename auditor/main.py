086c833e93b15925b5274db5b83053572aec63ca
0xЯ4Y <riadmouja47@gmail.com>
2026-02-21 10:21:28 +0100

    Update main.py

--- auditor/main.py
+++ auditor/main.py
@@ -1,1 +1,11 @@
+import os
+import sys
+from auditor.core.auditor import GCPAISecurityAuditor
 
+project_id = os.environ.get("GCP_PROJECT_ID")
+if project_id is None:
+    print("GCP_PROJECT_ID environment variable is not set.")
+    sys.exit(1)
+
+auditor = GCPAISecurityAuditor(project_id)
+auditor.run_all_checks()
